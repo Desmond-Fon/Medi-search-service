b9db0d55f390f8cbbcb5f34019c3b2d956e27146
Desmond-Fon <desmondfon@yahoo.com>
2023-07-20 16:09:04 +0100

    added new bg image

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,7 +1,8 @@
 import Nav from "../../components/Nav";
-import map from '../../assets/mapp.png'
+// import map from '../../assets/mapp.png'
 import { DataContext } from "../../contexts/Data";
 import { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 const Home = () => {
@@ -30,17 +31,17 @@ const Home = () => {
     console.log(userLocation);
 
     return (<>
-        <div>
+        <div className=' bg-hero h-screen bg-opacity-5'>
             <Nav />
             <div className="flex justify-between px-[120px] mt-[50px]">
-                <div className="flex flex-col items-start mt-[30px]">
-                    <h1 className="text-[60px] font-[700] leading-[76px] text-[#333] mb-[15px]">Healthcare search made easy </h1>
+                <div className="flex flex-col items-start mt-[30px]  p-10 bg-opacity-5 bg-white">
+                    <h1 className="text-[60px] font-[700] leading-[76px] text-[#333] mb-[15px]">Healthcare search <br /> made easy </h1>
                     <p className="text-[18px] w-[500px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.</p>
-                    <button className='px-[35px] py-[10px] rounded-[5px] bg-[#4762FF] text-[18px] font-[600] text-white mt-[32px]'>Try it out</button>
+                    <Link to='/login'><button className='px-[35px] py-[10px] rounded-[5px] bg-[#4762FF] text-[18px] font-[600] text-white mt-[32px]'>Try it out</button></Link>
 
                 </div>
                 <div>
-                    <img src={map} alt="" />
+                    {/* <img src={map} alt="" /> */}
                 </div>
             </div>
         </div>
